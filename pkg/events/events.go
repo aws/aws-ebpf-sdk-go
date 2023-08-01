75da01a58f400f9dda359afe83c36097810f0c77
Jayanth Varavani <1111446+jayanthvn@users.noreply.github.com>
2023-08-01 16:48:29 +0000

    PR nit

--- pkg/events/events.go
+++ pkg/events/events.go
@@ -51,7 +51,7 @@ func isValidMapFDList(mapFDlist []int) bool {
 		}
 		mapInfo, err := ebpf_maps.GetBPFmapInfo(mapFD)
 		if err != nil {
-			fmt.Errorf("failed to map info")
+			fmt.Errorf("failed to get map info")
 			return false
 		}
 		if mapInfo.Type != constdef.BPF_MAP_TYPE_RINGBUF.Index() {
@@ -85,7 +85,7 @@ func InitRingBuffer(mapFDlist []int) (map[int]chan []byte, error) {
 
 		mapInfo, err := ebpf_maps.GetBPFmapInfo(mapFD)
 		if err != nil {
-			fmt.Errorf("failed to map info")
+			fmt.Errorf("failed to get map info for mapFD %d", mapFD)
 			return nil, fmt.Errorf("failed to map info")
 		}
 
