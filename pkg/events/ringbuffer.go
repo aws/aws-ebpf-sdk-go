bf231d2c7cb42acd8565bce489b18ef7a09c334f
Jayanth Varavani <1111446+jayanthvn@users.noreply.github.com>
2023-08-01 06:44:12 +0000

    PR feedback

--- pkg/events/ringbuffer.go
+++ pkg/events/ringbuffer.go
@@ -15,187 +15,88 @@
 package events
 
 import (
-	"fmt"
-	"os"
-	"sync"
-	"syscall"
+	"encoding/binary"
+	"sync/atomic"
 	"unsafe"
 
-	constdef "github.com/aws/aws-ebpf-sdk-go/pkg/constants"
-	poller "github.com/aws/aws-ebpf-sdk-go/pkg/events/poll"
-	"github.com/aws/aws-ebpf-sdk-go/pkg/logger"
-	ebpf_maps "github.com/aws/aws-ebpf-sdk-go/pkg/maps"
 	"golang.org/x/sys/unix"
 )
 
-var log = logger.Get()
+var ringbufHeaderSize = binary.Size(ringbufHeader{})
+
+// ringbufHeader from 'struct bpf_ringbuf_hdr' in kernel/bpf/ringbuf.c
+type ringbufHeader struct {
+	Len   uint32
+	PgOff uint32
+}
+
+func memcpy(dst, src unsafe.Pointer, count uintptr) {
+	for i := uintptr(0); i < count; i++ {
+		b := *(*byte)(unsafe.Pointer(uintptr(src) + i))
+		*(*byte)(unsafe.Pointer(uintptr(dst) + i)) = b
+	}
+}
 
 type RingBuffer struct {
-	Rings                []*Ring
-	PageSize             int
-	RingCnt              int
-	stopRingBufferChan   chan struct{}
-	updateRingBufferChan chan *Ring
-	eventsStopChannel    chan struct{}
-	wg                   sync.WaitGroup
-	eventsDataChannel    chan []byte
-
-	epoller *poller.EventPoller
+	Consumerpos     unsafe.Pointer
+	Consumer        []byte
+	Producerpos     unsafe.Pointer
+	Producer        []byte
+	Mask            uint64
+	RingBufferMapFD int
+	Data            unsafe.Pointer
 }
 
-func InitRingBuffer(mapFD int) (<-chan []byte, error) {
-	if mapFD == -1 {
-		return nil, fmt.Errorf("invalid map FD")
-	}
-	mapInfo, err := ebpf_maps.GetBPFmapInfo(mapFD)
-	if err != nil {
-		return nil, fmt.Errorf("failed to map info")
-	}
-	log.Infof("Got map FD %d", mapFD)
-	if mapInfo.Type != constdef.BPF_MAP_TYPE_RINGBUF.Index() {
-		return nil, fmt.Errorf("unsupported map type, should be - BPF_MAP_TYPE_RINGBUF")
-	}
-
-	rb := &RingBuffer{
-		PageSize: os.Getpagesize(),
-		RingCnt:  0,
-	}
-
-	epoll, err := poller.NewEventPoller()
-	if err != nil {
-		return nil, fmt.Errorf("failed to create epoll instance: %s", err)
-	}
-	rb.epoller = epoll
-
-	eventsChan, err := rb.SetupRingBuffer(mapFD, mapInfo.MaxEntries)
-	if err != nil {
-		rb.CleanupRingBuffer()
-		return nil, fmt.Errorf("failed to add ring buffer: %s", err)
-	}
-	log.Infof("Ringbuffer setup done")
-	return eventsChan, nil
+func (r *RingBuffer) getConsumerPosition() uint64 {
+	return atomic.LoadUint64((*uint64)(r.Consumerpos))
 }
 
-func (rb *RingBuffer) SetupRingBuffer(mapFD int, maxEntries uint32) (<-chan []byte, error) {
-	ring := &Ring{
-		RingBufferMapFD: mapFD,
-		Mask:            uint64(maxEntries - 1),
-	}
-
-	// [Consumer page - 4k][Producer page - 4k][Data section - twice the size of max entries]
-	// Refer kernel code, twice the size of max entries will help in boundary scenarios
-	// https://github.com/torvalds/linux/blob/master/kernel/bpf/ringbuf.c#L125
-
-	tmp, err := unix.Mmap(mapFD, 0, rb.PageSize, unix.PROT_READ|unix.PROT_WRITE, unix.MAP_SHARED)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create Mmap for consumer -> %d: %s", mapFD, err)
-	}
-
-	ring.Consumerpos = unsafe.Pointer(&tmp[0])
-	ring.Consumer = tmp
-
-	mmap_sz := uint32(rb.PageSize) + 2*maxEntries
-	tmp, err = unix.Mmap(mapFD, int64(rb.PageSize), int(mmap_sz), unix.PROT_READ, unix.MAP_SHARED)
-	if err != nil {
-		unix.Munmap(tmp)
-		return nil, fmt.Errorf("failed to create Mmap for producer -> %d: %s", mapFD, err)
-	}
-
-	ring.Producerpos = unsafe.Pointer(&tmp[0])
-	ring.Producer = tmp
-	ring.Data = unsafe.Pointer(uintptr(unsafe.Pointer(&tmp[0])) + uintptr(rb.PageSize))
-
-	err = rb.epoller.AddEpollCtl(mapFD, rb.RingCnt)
-	if err != nil {
-		unix.Munmap(tmp)
-		return nil, fmt.Errorf("failed to Epoll event: %s", err)
-	}
-
-	rb.Rings = append(rb.Rings, ring)
-	rb.RingCnt++
-
-	//Start channels read
-	rb.eventsStopChannel = make(chan struct{})
-	rb.eventsDataChannel = make(chan []byte)
-
-	rb.wg.Add(1)
-	go rb.reconcileEventsDataChannel()
-	return rb.eventsDataChannel, nil
+func (r *RingBuffer) setConsumerPosition(newConsumerPosition uint64) {
+	atomic.StoreUint64((*uint64)(r.Consumerpos), newConsumerPosition)
 }
 
-func (rb *RingBuffer) CleanupRingBuffer() {
+func (r *RingBuffer) getProducerPosition() uint64 {
+	return atomic.LoadUint64((*uint64)(r.Producerpos))
 
-	for i := 0; i < rb.RingCnt; i++ {
-		_ = unix.Munmap(rb.Rings[i].Producer)
-		_ = unix.Munmap(rb.Rings[i].Consumer)
-		rb.Rings[i].Producerpos = nil
-		rb.Rings[i].Consumerpos = nil
-	}
-
-	if rb.epoller.GetEpollFD() >= 0 {
-		_ = syscall.Close(rb.epoller.GetEpollFD())
-	}
-	rb.epoller = nil
-	rb.Rings = nil
-	return
 }
 
-func (rb *RingBuffer) reconcileEventsDataChannel() {
+func (r *RingBuffer) ParseRingData(consumerPosition uint64) *RingData {
+	updateConsumerPosition := (uintptr(consumerPosition) & uintptr(r.Mask))
+	data := (*int32)(unsafe.Pointer(uintptr(r.Data) + updateConsumerPosition))
 
-	pollerCh := rb.epoller.EpollStart()
-	defer func() {
-		rb.wg.Done()
-	}()
+	//Get the len which is uint32 in header struct
+	headerLen := atomic.LoadInt32(data)
 
-	for {
-		select {
-		case bufferPtr, ok := <-pollerCh:
+	// Len in ringbufHeader has busy and discard bit so skip it
+	dataLen := (((uint32(headerLen) << 2) >> 2) + uint32(ringbufHeaderSize))
+	//round up dataLen to nearest 8-byte alignment
+	roundedDataLen := (dataLen + 7) &^ 7
 
-			if !ok {
-				return
-			}
-			rb.ReadRingBuffer(rb.Rings[bufferPtr])
-
-		case <-rb.eventsStopChannel:
-			return
-		}
+	ringdata := &RingData{
+		Data:      data,
+		HeaderLen: uint32(headerLen),
+		DataLen:   uint32(roundedDataLen),
 	}
+	return ringdata
 }
 
-// Similar to libbpf poll ring
-func (rb *RingBuffer) ReadRingBuffer(eventRing *Ring) {
-	readDone := true
-	consPosition := eventRing.getConsumerPosition()
-	for !readDone {
-		readDone = rb.parseBuffer(consPosition, eventRing)
-	}
+type RingData struct {
+	Data      *int32
+	HeaderLen uint32
+	DataLen   uint32
 }
 
-func (rb *RingBuffer) parseBuffer(consumerPosition uint64, eventRing *Ring) bool {
-	readDone := true
-	producerPosition := eventRing.getProducerPosition()
-	for consumerPosition < producerPosition {
+func (rd *RingData) isBusy() bool {
+	return (rd.HeaderLen & unix.BPF_RINGBUF_BUSY_BIT) != 0
+}
 
-		// Get the header - Data points to the DataPage which will be offset by consumerPosition
-		ringdata := eventRing.ParseRingData(consumerPosition)
+func (rd *RingData) isDiscard() bool {
+	return (rd.HeaderLen & unix.BPF_RINGBUF_DISCARD_BIT) != 0
+}
 
-		// Check if busy then skip, Might not be committed yet
-		// There are 2 steps -> reserve and then commit/discard
-		if ringdata.isBusy() {
-			readDone = true
-			break
-		}
-
-		readDone = false
-
-		// Update the position irrespective of discard or commit of data
-		consumerPosition += uint64(ringdata.DataLen)
-
-		//Pick the data only if committed
-		if !ringdata.isDiscard() {
-			rb.eventsDataChannel <- ringdata.parseSample()
-		}
-		eventRing.setConsumerPosition(consumerPosition)
-	}
-	return readDone
+func (rd *RingData) parseSample() []byte {
+	readableSample := unsafe.Pointer(uintptr(unsafe.Pointer(rd.Data)) + uintptr(ringbufHeaderSize))
+	dataBuf := make([]byte, int(rd.DataLen))
+	memcpy(unsafe.Pointer(&dataBuf[0]), readableSample, uintptr(rd.DataLen))
+	return dataBuf
 }
